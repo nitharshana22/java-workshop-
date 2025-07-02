0fbf69380ecb62c04da588d558baab70678125b0
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-02 14:57:47 +0530

    arrays

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array{
+    public static void main(String[]args){
+        int[]marks={80,90,99,98,89,00};
+        for(int mark : marks){
+            System.out.println(mark);
+        }
+    }
+}
