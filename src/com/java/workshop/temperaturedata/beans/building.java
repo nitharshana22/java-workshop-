724df68aabf03666b5ef9e24ae991ff91d8cc5bf
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-03 14:14:50 +0530

    building.java

---
+++ src/com/java/workshop/temperaturedata/beans/building.java
@@ -0,0 +1,10 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+import java.Time.Local.Data.Time;
+public class building {
+    private String name;
+    private Double area;
+    private int floor;
+    private LocalDataTime opendatatime;
+    private LocalDataTime closedatatime;
+}
