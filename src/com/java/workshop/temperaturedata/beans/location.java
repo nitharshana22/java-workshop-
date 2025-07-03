e245b405970369c069d5d81cd9a5da056b05ff4b
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-03 14:18:01 +0530

    location.java

---
+++ src/com/java/workshop/temperaturedata/beans/location.java
@@ -0,0 +1,19 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+public class location {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String description;
+    private String country;
+    private String city;
+    private String address;
+    private String postalcode;
+    public void setName(String aName){
+        name = aName;
+    }
+    public String getName(){
+         return name;
+    }
+}
+    
