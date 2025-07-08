60ad1f48227e971fc52a0f3bf5b7cfc12683fd96
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-08 11:02:21 +0530

    location.java

--- src/com/java/workshop/temperaturedata/beans/location.java
+++ src/com/java/workshop/temperaturedata/beans/location.java
@@ -15,5 +15,54 @@ public class location {
     public String getName(){
          return name;
     }
-}
+    public void setLatitude(Double aLatitude){
+        latitude = aLatitude;
+    }
+    public Double getLatitude(){
+        return latitude;
+    }
+    public void setLongitude(double aLongitude){
+        longitude=aLongitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setDescription(String aDescription){
+        description=aDescription;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setCountry(String aCountry){
+        country=aCountry;
+    }
+    public String getCountry(){
+        return country;
+    }
+    public void setCity(String aCity){
+        city=aCity;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setAddress(String aAddress){
+        address=aAddress;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setPostalcode(String aPostalcode){
+        postalcode=aPostalcode;
+    }
+    public String getPostalcode(){
+        return postalcode;
+    }
+    @Override
+    public String toString(){
+        return"Location { name:"+name+",Latitude:"+latitude+",
+               Longitude:"+longitude+",Description:"+description+",
+               Country:"+country+",City:"+city+",
+                Address:"+address+",Postalcode"};
+    }
+    }
     
