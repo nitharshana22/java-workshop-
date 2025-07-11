b2d0e35e2a9f9d0cfb0babf7dc04dcea3c2b15be
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-11 14:20:12 +0530

    StreamupperCaseNames.java

---
+++ src/main/java/com/java/workshop/StreamUpperCaseNames.java
@@ -0,0 +1,20 @@
+package com.java.workshop;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamUpperCaseNames {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("pramisha");
+        names.add("dhanush");
+        names.add("dhanushya");
+        names.add("rishika");
+        names.add("shrika");
+        names.add("sri vignesh");
+        names.stream()
+        .filter(name -> name.startsWith("s") || name.startsWith("s"))
+        .distinct().map(String::toUpperCase).forEach(System.out::println);
+       
+    }
+   
+}
