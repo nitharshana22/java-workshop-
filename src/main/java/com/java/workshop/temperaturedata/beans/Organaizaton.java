e2b7a2e823bbb0ef230c79fdb9f4d00e6ea7dfc0
nitharshana22 <nitharshanag24cse@vetias.ac.in>
2025-07-14 10:51:25 +0530

    Organaization.java

---
+++ src/main/java/com/java/workshop/temperaturedata/beans/Organaizaton.java
@@ -0,0 +1,12 @@
+package com.java.workshop.temperaturedata.beans;
+
+
+public record Organaizaton(String name,
+    String completeAddress,
+    String website,
+    String email,
+    String contactnumber,
+    long registrationNumber
+    ){
+
+    }
